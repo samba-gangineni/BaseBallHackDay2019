01caa6b99d554368389859ec75c81b6bc2e6994b
samba693 <sambasivarao693@gmail.com>
2019-03-09 13:40:29 -0500

    schdulels in csv and xml 2018

--- get_schedule.py
+++ get_schedule.py
@@ -9,12 +9,12 @@ config_file = json.load(open('config.json','r'))
 my_key = config_file.get("my_key")
 
 # Requesting the data
-conn.request("GET","/mlb-t6/games/2019/REG/schedule.xml?api_key={}".format(my_key))
+conn.request("GET","/mlb-t6/games/2018/REG/schedule.xml?api_key={}".format(my_key))
 
 # Reading in the data
 res = conn.getresponse()
 data = res.read()
 
 # Saving the response
-with open('schedule.xml','wb') as f:
+with open('schedule_2018.xml','wb') as f:
     f.write(data)
